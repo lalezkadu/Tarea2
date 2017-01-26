953ec2232bb38d71c8bb9f2929ce947971495b75
Lalezka Duque <lalezkaduque@gmail.com>
2017-01-26 17:51:12 -0400

    Update calcularPrecio.py

--- calcularPrecio.py
+++ calcularPrecio.py
@@ -9,9 +9,15 @@ import math
 import datetime
 import sys
 
+# Clase Tarifa
+# Almacena las tarifas correspondientes a la hora de servicio en los días de
+# la semana
+# Atributos:
+# ds: tarifa de los dias entre semana
+# fs: tarfia de los fines de semana
 class Tarifa():
-	def __init__(self,ds,fs):
-		if((type(ds) is int or type(ds) is float) 
+	def __init__(self, ds, fs):
+		if ((type(ds) is int or type(ds) is float) 
 			and (type(fs) is int or type(fs) is float)):
 			self.diasemana = decimal.Decimal(ds)
 			self.finsemana = decimal.Decimal(fs)
@@ -19,90 +25,99 @@ class Tarifa():
 			self.diasemana = -1
 			self.finsemana = -1
 
-# Intervalo en horas: [1,168]
-# tiempoDeServicio = [inicio_servicio,fin_servicio]
+# Determina el 
+# Parametros:
+# tarifa: instancia de la clase Tarifa que contiene las tarifas por hora de 
+# servicio
+# tiempoDeServicio: Lista que contiene el tiempo de inicio del servicio y
+# el tiempo en el cual termino el servicio
+# Retorna el precio total por el tiempo de servicio.
 def calcularPrecio(tarifa, tiempoDeServicio):
-	if(tarifa.diasemana < 0 or tarifa.finsemana < 0):
+	# Las tarifas son numeros enteros positivos
+	if (tarifa.diasemana < 0 or tarifa.finsemana < 0):
 		return -1
 
-	precio = 0
-	inicio = tiempoDeServicio[0]
-	fin = tiempoDeServicio[1]
-	tiempo_total = fin - inicio
-	dias_servicio = tiempo_total.days()
-	horas_servicio = tiempo_total.days()*24 + tiempo_total.seconds()//3600
-	segundos_servicio = tiempo_total.seconds()
-	dia_inicio = inicio.weekday()
-	dia_fin = fin.weekday()
+	precio = 0																# Precio total inicial
+	inicio = tiempoDeServicio[0]											# Inicio del servicio
+	fin = tiempoDeServicio[1]												# Fin del servicio
+
+	tiempo_total = fin - inicio 											# Tiempo total de servicio
+	dias_servicio = tiempo_total.days										# Tiempo de servicio en dias
+	horas_servicio = tiempo_total.days*24+tiempo_total.seconds//3600		# Tiempo de servicio en horas
+	segundos_servicio = tiempo_total.seconds								# Tiempo de servicio en segundos
+	dia_inicio = inicio.weekday()											# Dia en el cual inicio el servicio
+	dia_fin = fin.weekday()													# Dia en el cual finalizo el servicio
 
 	# Redondeamos las horas de servicio
-	if(segundos_servicio % 3600 > 0):
+	if (segundos_servicio % 3600 > 0):
 		horas_servicio += 1
-	if(dia_inicio == 4 and dia_fin == 0):
+	if (dia_inicio == 4 and dia_fin == 0):
 		horas_servicio += 1
 
-	if(dias_servicio == 0 and segundos_servicio < 900):
+	if (dias_servicio == 0 and segundos_servicio < 900):	# Verificamos que el tiempo de servicio se encuentra en el dominio
 		print("El tiempo de servicio fue menor a 15 minutos.")
 		return -1
-	elif(dias_servicio > 7):
+	elif (dias_servicio > 7):
 		print("El tiempo de servicio fue mayor a 7 dias")
 		return -1
-	elif(((dia_inicio == 4 and dia_fin == 5) or
-		(dia_inicio == 6 and dia_fin == 0)) and horas_servicio == 1):
-		
-		if(dia_inicio == 5 or dia_inicio == 6):
+	elif (((dia_inicio == 4 and dia_fin == 5) 				# Calculo del precio
+		or (dia_inicio == 6 and dia_fin == 0)
+		or (dia_inicio == 5 and dia_fin == 6)) 				# Servicio de una hora en fin de semana
+		and horas_servicio == 1):
+		if (dia_inicio == 5 or dia_inicio == 6):			# Fin de semana
 			precio += tarifa.finsemana
-		else:
+		else:												# Entre semana
 			precio += tarifa.diasemana
 		
-		if(dia_fin == 5 or dia_fin == 6):
+		if (dia_fin == 5 or dia_fin == 6):					# Fin de semana
 			precio += tarifa.finsemana
-		else:
+		else:												# Entre semana
 			precio += tarifa.diasemana
 	else:
-		
-		if(inicio.day == fin.day):
-			
-			if(dia_inicio == 5 or dia_inicio == 6):
+		if (inicio.day == fin.day):							# Servicio de un dia
+			if (dia_inicio == 5 or dia_inicio == 6):		# Fin de semana
 				precio += decimal.Decimal(horas_servicio*tarifa.finsemana)
-			else:
+			else:											# Entre semana
 				precio += decimal.Decimal(horas_servicio*tarifa.diasemana)
-		else:
+		else:												# Servicio de varios dias de duracion
+			if (dia_inicio == 5 or dia_inicio == 6):		# Servicio comienza fin de semana
+				precio += decimal.Decimal((24-inicio.hour)*tarifa.finsemana)
+			else:											# Servicio comienza entre semana
+				precio += decimal.Decimal((24-inicio.hour)*tarifa.diasemana)
 			
-			if(dia_inicio == 5 or dia_inicio == 6):
-				precio += decimal.Decimal((24-inicio.hour())*tarifa.finsemana)
-			else:
-				precio += decimal.Decimal((24-inicio.hour())*tarifa.diasemana)
-			
-			horas_restantes = horas_servicio
-			horas_restantes -= (24-inicio.hour())
-    
-			if(dia_inicio == 6):
+			horas_restantes = horas_servicio		
+			horas_restantes -= (24-inicio.hour)			# Quitamos las horas ya calculadas
+    		
+    		# Actualizamos el dia siguiente
+    		# Si comenzamos en Domingo, seguimos calculando desde el Lunes
+			if (dia_inicio == 6):
 				dia_actual = 0
 			else:
-				dia_actual = dia_inicio + 1
-			if(horas_restantes < 24 and horas_restantes > 0):
+				dia_actual = dia_inicio+1
+
+			# Redondeo de los dias restantes de servicio
+			if (horas_restantes < 24 and horas_restantes > 0):
 				dias_restantes = 1
 			else:
 				dias_restantes = math.ceil(horas_restantes/24)
-                
-			while(dias_restantes > 0):
+			# Iterando sobre los dias en los cuales se hizo servicio calculamos
+            # el resto del precio hasta agotar los dias restantes por calcular
+			while (dias_restantes > 0):
 
-				if(dias_restantes == 1):
-				
-					if(dia_actual == 5 or dia_actual == 6):
+				if (dias_restantes == 1):			
+					if (dia_actual == 5 or dia_actual == 6):
 						precio += decimal.Decimal(horas_restantes*tarifa.finsemana)
 					else:
 						precio += decimal.Decimal(horas_restantes*tarifa.diasemana)
 				else:
 					horas_restantes -= 24
 				
-					if(dia_actual == 5 or dia_actual == 6):
+					if (dia_actual == 5 or dia_actual == 6):
 						precio += decimal.Decimal(24*tarifa.finsemana)
 					else:
 						precio += decimal.Decimal(24*tarifa.diasemana)
                         
-				if(dia_actual == 6):
+				if (dia_actual == 6):
 					dia_actual = 0
 				else:
 					dia_actual += 1
